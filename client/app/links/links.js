b45acb7d9f4992be91f76ed1cd30d36c3b7b0d3c
Ansel Rosenberg <ansel01@gmail.com>
2015-04-23 12:19:22 -0700

    Add default stying

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -9,11 +9,22 @@ angular.module('shortly.links', [])
     });
   };
 
-  // $scope.addCount = function(link) {
-  //   Links.addCount(link).then(function(data) {
-  //     $scope.data.links = data;
-  //   });
-  // }
-
   $scope.getLinks();
+})
+
+.directive('ngLinks', function() {
+  return {
+    restrict: 'EA',
+    template: '<div class="link" ng-repeat="link in data.links | filter:searchText | orderBy: \'-visits\'"> \
+    <img src=\'../assets/redirect_icon.png\'/> \
+    <div class="info"> \
+      <div style="display: inline;"><a href="{{ \'api/links/\' + link.code }}">{{ link.code }}</a></div> \
+      <div style="display: inline;">{{ link.url }}</div> \
+      <div class="visits" ><span class="count">{{ link.visits }}</span></div> \
+    </div> \
+    </div>'
+
+  }
 });
+
+   
